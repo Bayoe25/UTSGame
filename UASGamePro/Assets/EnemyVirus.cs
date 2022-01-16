e10486e8153c531f62f8ececefe1fb7ce40ab441
Bayoe25 <dimaspangestu871@gmail.com>
2022-01-17 01:11:25 +0700

    Uas

---
+++ UASGamePro/Assets/EnemyVirus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVirus : MonoBehaviour
+{
+    public GameObject enemy;
+    public float timeMinimal, timeMaximal;
+    public float minimalPosition, maximalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(VirusEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator VirusEnemy()
+    {
+        Instantiate(enemy, transform.position + Vector3.right * Random.Range(minimalPosition,maximalPosition)
+        , Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(timeMinimal,timeMaximal));
+        StartCoroutine(VirusEnemy());
+    }
+}
---
+++ UASGamePro/Readme.txt
@@ -0,0 +1,8 @@
+Nama 	: Bayu Pangestu
+Nim 	: A11.2018.11031
+
+Note Game Play :
+
+-> : Menggerakan Player Ke Kanan
+<- : Menggerakan Player Ke Kiri
+R  : Untuk Restart ketika Player Terkena Virus
